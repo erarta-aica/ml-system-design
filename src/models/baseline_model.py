a0dcfdbc4c7ca406e47740678fd7d37ccc38a830
Evgeniy Dubskiy <evgeniy@erarta.ai>
2025-01-20 18:06:21 +0400

    Added baseline training

---
+++ src/models/baseline_model.py
@@ -0,0 +1,17 @@
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+
+def build_baseline_model(input_shape, num_classes):
+    base_model = ResNet50(weights='imagenet', include_top=False, input_shape=input_shape)
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1024, activation='relu')(x)
+    predictions = Dense(num_classes, activation='softmax')(x)
+    model = Model(inputs=base_model.input, outputs=predictions)
+    
+    for layer in base_model.layers:
+        layer.trainable = False
+    
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
